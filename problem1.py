ba347961d183c1a70a8626bcd8939bbada797edf
Ted Watts <tawatts1@asu.edu>
2020-03-19 17:28:53 -0700

    deleted unused file

--- problem1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from outerplanets import *
-
-Ur0 = initial_position(theta0['Uranus'], distance['Uranus'])
-Nr0 = initial_position(theta0['Neptune'], distance['Neptune'])
